e0bb07717feaf08ef1aee70529b6c08f0b2de6ab
DmitryTevtonsky <tevtonsky1054m1@gmail.com>
2022-06-27 20:05:26 +0300

    Styles for layout.

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,18 +1,20 @@
 import { useData } from 'hooks';
 import React, { FC } from 'react';
 
+import Main from 'features/main';
+
 import css from './index.module.css';
 
 const Core: FC = () => {
   const { data, loading, error } = useData();
 
-  console.log({ data, loading, error });
-
   return (
     <section className={css.layout}>
-      <header>HEADER</header>
-      <main>MAIN</main>
-      <footer>FOOTER</footer>
+      <header className={css.header}>SpaceX launches virtualized list</header>
+      <main className={css.main}>
+        <Main loading={loading} error={error} data={data} />
+      </main>
+      <footer className={css.footer}>FOOTER</footer>
     </section>
   );
 };
